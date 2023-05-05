26d0a2dbd017f1cc77fe86cf3bf583b291b0a9b6
showkhin1997 <showkhin15@gmail.com>
2023-05-05 19:38:13 +0600

    seerToMon

---
+++ seer-to-mon.js
@@ -0,0 +1,17 @@
+function seerToMon(seer) {
+    // check validation
+    if (seer == -1) {
+        return "Please give positive number";
+    }
+    else if (seer != "number") {
+        return "Please give a numeric number";
+    }
+    // converting calculation
+    const perSeer = 40;
+    let mon = seer / perSeer;
+    return mon;
+}
+
+const givenSeer = 20;
+const result = seerToMon(givenSeer);
+console.log(result);
